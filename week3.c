650b4ecf67c4f32e35897646d91f9ee79ccc6d41
binhbinh22 <81414662+binhbinh22@users.noreply.github.com>
2021-04-05 11:22:45 +0700

    Add files via upload

---
+++ week3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+printf ("%d\n", 455);
+printf ("%d\n", 455);
+printf ("%d\n ",455);
+printf ("%hd\n", 20000);
+char character = 'A';
+char string []= "this is a string";
+const char *stringPtr="this is also a string";
+printf ("%c\n", character);
+printf ("%s\n", "this is a string");
+printf ("%s/n", string);
+printf ("%s\n", stringPtr);
+return 0;
+}
+
